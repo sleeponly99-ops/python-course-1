8533c8b95e5d18245b81284b4288539b96ebadd4
sleeponly99-ops <sleeponly99@gmail.com>
2026-02-11 22:04:26 +0500

    using and or operaters

---
+++ lesson6_activities/lesson6_activity1.py
@@ -0,0 +1,15 @@
+x=9
+y=11
+z=0
+if x and y and z:
+    print("boolean values are true")
+else:
+    print("boolean values are false")
+
+a=4
+b=5
+c=6
+if a > 7 or b > 7:
+    print("a and b are greater than 7")
+else:
+    print("a and b are not greater than 7")
