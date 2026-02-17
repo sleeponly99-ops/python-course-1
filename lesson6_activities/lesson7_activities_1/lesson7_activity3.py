9b012057d1090dfa14ed0f4a86c618846693fc5e
sleeponly99-ops <sleeponly99@gmail.com>
2026-02-17 21:54:21 +0500

    grading system

---
+++ lesson6_activities/lesson7_activities_1/lesson7_activity3.py
@@ -0,0 +1,11 @@
+math=40
+english=45
+urdu=37
+total=math+english+urdu
+average=total/3
+if average>= 91 and average<=100:
+    print("you are graded A1")
+elif average>=81 and average<91:
+    print("you are in grade A2")
+else:
+    print("try to achieve next time")
