86189eb534240f85980da28e93a596c3f5d4e21f
sleeponly99-ops <sleeponly99@gmail.com>
2026-02-17 21:40:34 +0500

    identity operators

---
+++ lesson7_activities/lesson7_activity1.py
@@ -0,0 +1,18 @@
+x=6
+if (type(x) is int ):
+    print("true")
+else:
+    print("false")
+
+y=5.5
+if (type(y) is float):
+    print("its float")
+else:
+    print("not float")
+
+a=10
+b=20
+if a is b:
+    print("a and b are same")
+else:
+    print("a and b are not same")
